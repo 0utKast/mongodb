71f1e5425c96bffd034fa2d29eaee6981c0d4ec3
0utKast <educaleft@gmail.com>
2013-03-02 11:15:10 +0000

    vt10_mongodb
    Archivos prácticas VT 10 mongodb

---
+++ vt10_curso_mongodb/PhpSesionesMongoDB/dbconnection.php
@@ -0,0 +1,38 @@
+<?php
+
+class DBConnection {
+
+    const HOST = 'localhost';
+    const PORT = 27017;
+    const DBNAME = 'miblog';
+
+    private static $instance;
+    public $connection;
+    public $database;
+
+    private function __construct() {
+        $connectionString = sprintf('mongodb://%s:%d', DBConnection::HOST, DBConnection::PORT);
+
+        try {
+
+            $this->connection = new Mongo($connectionString);
+            $this->database = $this->connection->selectDB(DBConnection::DBNAME);
+        } catch (MongoConnectionException $e) {
+            throw $e;
+        }
+    }
+
+    static public function instantiate() {
+        if (!isset(self::$instance)) {
+            $class = __CLASS__;
+            self::$instance = new $class;
+        }
+
+        return self::$instance;
+    }
+
+    public function getCollection($name) {
+        return $this->database->selectCollection($name);
+    }
+
+}
